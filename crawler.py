e97943af54b2cbcd8746d72358e0455fe170b90d
ahwitz <ahwitz@gmail.com>
2015-02-01 11:54:51 -0500

    Fixed I/O

--- crawler.py
+++ crawler.py
@@ -6,38 +6,51 @@ import re, operator
 from contextlib import closing
 from selenium.webdriver import Firefox
 
-artistName = "Cake"
-artistPage = urllib2.urlopen("http://lyrics.wikia.com/api.php?func=getArtist&artist=" + artistName + "&fmt=realjson")
-artistAlbums = json.load(artistPage)['albums']
-songs = []
-words = {}
+artist_names = []
 
-for curAlbum in artistAlbums:
-    for curSong in curAlbum['songs']:
-        songs.append(curSong)
+with open("artist_names.txt", "r") as artist_file:
+    artist_names = artist_file.readlines()
+
+print artist_names
 
 with closing(Firefox()) as browser:
-    for curSong in songs:
-        print "Loading page..."
-        browser.get("http://lyrics.wikia.com/" + artistName + ":" + curSong)
-        print "Page loaded..."
-        lyricbox = browser.find_elements_by_class_name("lyricbox")
-        
-        try:
-            curWords = lyricbox[0].text.split("\n")
-        except IndexError:
-            continue
+    for cur_artist in artist_names:
+        cur_artist = cur_artist.strip()
+        print "Starting histogram for " + cur_artist
+        url = "http://lyrics.wikia.com/api.php?func=getArtist&artist=" + cur_artist + "&fmt=realjson"
+        print url
+        artistPage = urllib2.urlopen(url)
+        artistAlbums = json.load(artistPage)['albums']
+        songs = []
+        words = {}
 
-        for curLine in curWords:
-            splitWords = curLine.split(" ")
+        for curAlbum in artistAlbums:
+            for curSong in curAlbum['songs']:
+                songs.append(curSong)
 
-            for curWord in splitWords:
-                if words.has_key(curWord):
-                    words[curWord] += 1
-                else:
-                    words[curWord] = 1
+            for curSong in songs:
+                print "Loading song " + curSong
+                browser.get("http://lyrics.wikia.com/" + cur_artist + ":" + curSong)
+                lyricbox = browser.find_elements_by_class_name("lyricbox")
+                
+                try:
+                    curWords = lyricbox[0].text.split("\n")
+                except IndexError:
+                    continue
 
-sorted_words = sorted(words.items(), key=operator.itemgetter(1))
+                for curLine in curWords:
+                    splitWords = curLine.split(" ")
 
-print sorted_words
+                    for curWord in splitWords:
+                        if words.has_key(curWord):
+                            words[curWord] += 1
+                        else:
+                            words[curWord] = 1
+
+        sorted_words = sorted(words.items(), key=operator.itemgetter(1))
+
+        with open(cur_artist + ".json", "w") as artist_out_file:
+            json.dump(sorted_words, artist_out_file)
+
+        print sorted_words
 
