60e4c926cfa8c8abc1c954eec48caeca6a0f181c
Josh Moore <djlamar@dhcp-rhodes-1396.redrover.cornell.edu>
2015-02-01 17:11:28 -0500

    Moved fetching into a function

--- fetch_lyrics_and_process.py
+++ fetch_lyrics_and_process.py
@@ -3,18 +3,22 @@ import embed
 import sys
 import pickle
 
-with open(sys.argv[1]) as f:
-  artists = [l.strip() for l in f]
-songs = []
+def fetch_and_process_artist_file(filename):
+  with open(filename) as f:
+    artists = [l.strip() for l in f]
+  songs = []
 
-for a in artists:
-  songs += crawler.getLyrics(a)
+  for a in artists:
+    songs += crawler.getLyrics(a)
 
-with open(sys.argv[1] + ".songs_pickle", "w") as fout:
-  pickle.dump(songs, fout)
+  with open(filename + ".songs_pickle", "w") as fout:
+    pickle.dump(songs, fout)
 
 # Gets a map from artist then word to word occurrence
-lyrics_map = crawler.processLyrics(songs)
+  lyrics_map = crawler.processLyrics(songs)
 
-with open(sys.argv[1] + ".lyrics_map_pickle", "w") as fout:
-  pickle.dump(lyrics_map, fout)
+  with open(filename + ".lyrics_map_pickle", "w") as fout:
+    pickle.dump(lyrics_map, fout)
+
+if __name__ == "__main__":
+  fetch_and_process_artist_file(sys.argv[1])
